f6a4f9b8edffd427467c9eea12933112d8dfa883
Laurence Pirard <55586017+Laurence2712@users.noreply.github.com>
2025-11-06 15:23:00 +0100

    corr

--- config/database.js
+++ config/database.js
@@ -1,9 +1,16 @@
-module.exports = ({ env }) => ({
-  connection: {
-    client: env('DATABASE_CLIENT', 'sqlite'),
+module.exports = ({ env }) => {
+  // debug: print selected DB env values when Strapi loads this file
+  // (this will appear in the Strapi startup logs)
+  console.log('[config/database] DATABASE_CLIENT=', env('DATABASE_CLIENT', 'sqlite'));
+  console.log('[config/database] DATABASE_FILENAME=', env('DATABASE_FILENAME', '.tmp/data.db'));
+
+  return {
     connection: {
-      filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+      client: env('DATABASE_CLIENT', 'sqlite'),
+      connection: {
+        filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+      },
+      useNullAsDefault: true,
     },
-    useNullAsDefault: true,
-  },
-});
+  };
+};
